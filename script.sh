f3d52dc41b393b5e0586848ebb578db5eb6d92a4
anjgola <anjali.gola@gmail.com>
2017-08-07 19:01:07 -0700

    Updating script to get rapidjson

--- script.sh
+++ script.sh
@@ -2,6 +2,9 @@
 rsync -av include/ src/
 #Rename samples as examples
 mv samples examples
+#Remove websocket and mbedtls
+rm -rf network/MbedTLS
+rm -rf network/WebSocket
 #Create mega header file
 #search_dir=src/
 #for entry in "$search_dir"*; do
@@ -10,7 +13,7 @@ mv samples examples
 #      echo "#include \"${entry#$search_dir}\"" >> src/aws-iot-device-sdk-cpp.h
 #  fi
 ##done
-find src -name *.hpp | sed -n 's|^src/|#include\ "|pw src/aws-iot-device-sdk-cpp.h'
+find src -name *.hpp | sed -n 's|^src/|#include\ "|p' | sed 's/$/"/' | sed 'w src/aws-iot-device-sdk-cpp.h'
 #Remove include folder
 rm -rf include/
 #Add corei7 folder to support load flags
@@ -28,3 +31,11 @@ mkdir extras
 mv *.md extras/
 mv LICENSE extras/
 mv NOTICE extras/
+#Get rapidJson and its license
+git clone https://github.com/miloyip/rapidjson.git tmp
+mv tmp/include/rapidjson src/
+mv tmp/license.txt src/rapidjson/
+rm -rf tmp
+#Get the discovery Arduino sample TODO
+
+
